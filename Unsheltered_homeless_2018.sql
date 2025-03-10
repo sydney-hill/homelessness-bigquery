eb6fddc74594a73bd0c7f52236abf9c3e8ecdca1
sydney-hill <sydney.hill130@gmail.com>
2025-03-10 13:56:21 -0500

    quereis_homelessness_2018.sql

--- Unsheltered_homeless_2018.sql
+++ Unsheltered_homeless_2018.sql
@@ -18,3 +18,28 @@ GROUP BY State
 ORDER BY Total_Homeless DESC
 LIMIT 7
 
+
+SELECT CoC_Name as Sheltered_in_SH_Locations, Sheltered_SH_Homeless as number_of_homeless
+FROM `tranquil-well-447022-n8.exploration_project.homelessness`
+WHERE Sheltered_SH_Homeless > 0 AND Count_Year = 2018
+ORDER BY Sheltered_SH_Homeless DESC
+LIMIT 3
+
+
+SELECT COUNT(CoC_Name) as Sheltered_in_SH_Locations
+FROM `tranquil-well-447022-n8.exploration_project.homelessness`
+WHERE Sheltered_SH_Homeless > 0
+AND Count_Year = 2018
+
+
+SELECT Unsheltered_Homeless, Count_Year
+FROM `tranquil-well-447022-n8.exploration_project.homelessness`
+WHERE State = 'DE'
+ORDER BY Count_Year
+
+
+SELECT CoC_Name
+FROM `tranquil-well-447022-n8.exploration_project.homelessness`
+WHERE Count_Year = 2018
+ORDER BY Homeless_Unaccompanied_Youth_Under_18 DESC
+LIMIT 3
