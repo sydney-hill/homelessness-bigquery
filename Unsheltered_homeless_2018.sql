0f430eb284d1786e49572c7541dbe745d9601e64
sydney-hill <sydney.hill130@gmail.com>
2025-03-10 13:54:20 -0500

    Update Unsheltered_homeless_2018.sql

--- Unsheltered_homeless_2018.sql
+++ Unsheltered_homeless_2018.sql
@@ -7,3 +7,14 @@ WHERE Overall_Homeless > 1000
 AND Unsheltered_Homeless < 100
 AND Count_Year = 2018 
 AND Unsheltered_Homeless < (Overall_Homeless*0.02)
+
+
+SELECT 
+  State,
+  SUM(Overall_Homeless) AS Total_Homeless
+FROM `tranquil-well-447022-n8.exploration_project.homelessness`
+WHERE Count_Year = 2018
+GROUP BY State
+ORDER BY Total_Homeless DESC
+LIMIT 7
+
