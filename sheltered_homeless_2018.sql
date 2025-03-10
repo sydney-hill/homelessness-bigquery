e2de5e8e6bdb4a827f1c30aa691a885bc8ed5729
sydney-hill <sydney.hill130@gmail.com>
2025-03-10 14:26:22 -0500

    Delete sheltered_homeless_2018.sql

--- sheltered_homeless_2018.sql
+++
@@ -1,5 +0,0 @@
-SELECT CoC_Name as Sheltered_in_SH_Locations, Sheltered_SH_Homeless as number_of_homeless
-FROM `tranquil-well-447022-n8.exploration_project.homelessness`
-WHERE Sheltered_SH_Homeless > 0 AND Count_Year = 2018
-ORDER BY Sheltered_SH_Homeless DESC
-LIMIT 3
