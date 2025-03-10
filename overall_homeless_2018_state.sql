eef34aa3d2237750b7766263c25f3434e038bf3f
sydney-hill <sydney.hill130@gmail.com>
2025-03-10 14:26:29 -0500

    Delete overall_homeless_2018_state.sql

--- overall_homeless_2018_state.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-  State,
-  SUM(Overall_Homeless) AS Total_Homeless
-FROM `tranquil-well-447022-n8.exploration_project.homelessness`
-WHERE Count_Year = 2018
-GROUP BY State
-ORDER BY Total_Homeless DESC
-LIMIT 7
